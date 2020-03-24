038c139e3ddebf671d7d987b8126f9b88c555769
FireWork2020 <guoyuxiang13123@163.com>
2020-03-24 17:29:01 +0800

    DynamicProxy

--- src/per/gyx/Main.java
+++ src/per/gyx/Main.java
@@ -1,14 +1,36 @@
 package per.gyx;
 
+import java.lang.reflect.InvocationHandler;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int arr[] = new int[]{3, 2, 1, 5, 2, 3, 3, 3, 6, 4};
-        System.out.println("准备排序");
-        new QuickSort().quickSort(arr, 0, arr.length - 1);
-        for (int temp : arr) {
-            System.out.println(temp);
+        ArrayList list = new ArrayList<>();
+        list.add(1);
+        list.add("abc");
+        System.out.println(list.get(0).getClass());
+        System.out.println(list.get(1).getClass());
+        System.out.println(list.getClass());
+
+        Lock lock = new ReentrantLock();
+        try {
+            lock.lock();
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
+
+
+    }
+
+    public static void test() {
+
     }
 }
