7615e2379753ceae78da48d7b01d63821a65d1a6
FireWork2020 <guoyuxiang13123@163.com>
2020-03-26 22:46:29 +0800

    MeiTuanTest

--- src/per/gyx/Main.java
+++ src/per/gyx/Main.java
@@ -1,36 +1,45 @@
 package per.gyx;
 
-import java.lang.reflect.InvocationHandler;
+
 import java.util.ArrayList;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        ArrayList list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        List<Integer> list = new ArrayList<>(50);
         list.add(1);
-        list.add("abc");
-        System.out.println(list.get(0).getClass());
-        System.out.println(list.get(1).getClass());
-        System.out.println(list.getClass());
-
-        Lock lock = new ReentrantLock();
-        try {
-            lock.lock();
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
+        for (int i = 6; i <= num; i++) {
+            int temp = 0;
+            for (int j = 0; j < list.size(); j++) {
+                int t = list.get(j);
+                t *= i;
+                t += temp;
+                temp = t / 10;
+                list.set(j, t % 10);
+            }
+            if (temp != 0) {
+                list.add(temp);
+            }
         }
-
-
+        int temp = 0;
+        for (int i = 0; i < list.size(); i++) {
+            int t = list.get(i);
+            t *= 2;
+            t += temp;
+            temp = t / 10;
+            list.set(i, t % 10);
+        }
+        if (temp != 0) {
+            list.add(temp);
+        }
+        for (int j = list.size() - 1; j >= 0; j--) {
+            System.out.print(list.get(j));
+        }
     }
 
-    public static void test() {
-
-    }
 }
